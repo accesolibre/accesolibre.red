fc22da6cd1a526fc0929e0d3e25a5665ea6d5d6e
Jose Luis Rivas <me@ghostbar.co>
2014-05-28 22:18:12 -0500

    no csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -362,8 +362,8 @@ module.exports = function (grunt) {
     'clean:server',
     'jekyll:check',
     'compass:server',
-    'jshint:all',
-    'csslint:check'
+    'jshint:all'
+    //'csslint:check'
   ]);
 
   grunt.registerTask('build', [
