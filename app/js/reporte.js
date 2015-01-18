cf70d29596213b9632f94f6474daff030f190b93
Sebastián Magrí <sebasmagri@gmail.com>
2015-01-18 13:30:47 -0430

    Consider jshint dot notation guidelines

--- app/js/reporte.js
+++ app/js/reporte.js
@@ -17,14 +17,14 @@
     if (captchaIsValid) {
       (function() {
         var reportes = new Firebase('https://reportes-accesolibre.firebaseio.com/reportes/'),
-            tipos = form.elements['tipo_falla'],
+            tipos = form.elements.tipo_falla,
             indexTipo = tipos.selectedIndex,
             tipo = tipos.options[indexTipo].value;
         reportes.push({
           tipo: tipo,
-          ubicacion: form.elements['ubicacion'].value,
-          tiempo: form.elements['tiempo'].value,
-          detalles: form.elements['detalles'].value
+          ubicacion: form.elements.ubicacion.value,
+          tiempo: form.elements.tiempo.value,
+          detalles: form.elements.detalles.value
         }, function() {
           document.querySelector('.al-form').style.display = 'none';
           document.querySelector('.gracias').style.display = 'block';
